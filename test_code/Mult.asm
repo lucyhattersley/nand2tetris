42521f72cd73f63800db8e000a3915c0994df450
lucyhattersley <lucy.a.hattersley@gmail.com>
2021-02-19 19:37:34 +0000

    Increasing but falls one short

--- test_code/Mult.asm
+++ test_code/Mult.asm
@@ -1,28 +1,22 @@
 // Multiplies R0 and R1
 // This code adds two numbers
-    @R1   // A -> R0
-    D=M   // D -> M[A]
-
-    @sum  // A -> R16
-    M=D   // Set R16 to value at D
-
 (LOOP)    // line 3 link for loop
     // check R1 > 0. If R1 <=0 GOTO END  
-    @R1
+    @R1   // A-R1
     D=M   // D -> M[A]
 
-    @END
-    D;JLE
+    @END  // Point to END
+    D;JLE // Jump to end if D<=0
 
     // Get first value
     @R0   // A -> R0
     D=M   // D->M[A]
 
     // Add first value to sum (should be)
-    @sum   // A -> R16
+    @sum  // A -> R16
     D=D+M // D -> D+A
 
-    // Store combined value
+    // Store value
     @sum  // A -> R16
     M=D   // M[A] -> R16
 
@@ -37,7 +31,7 @@
     0;JMP // Jumps to loop
 
 (END)     // Infinite loop
-    @LOOP  // Points to END location
+    @END  // Points to END location
     0;JMP // Jumps to END location
 
 
