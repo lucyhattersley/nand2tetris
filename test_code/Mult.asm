d70cb78f7aaa0f9dd58f61267c64ad0a99d9b4fa
lucyhattersley <lucy.a.hattersley@gmail.com>
2021-02-19 19:08:18 +0000

    infinite loop. Adding correctly

--- test_code/Mult.asm
+++ test_code/Mult.asm
@@ -1,14 +1,18 @@
 // Multiplies R0 and R1
 // This code adds two numbers
+    @R1   // A -> R0
+    D=M   // D -> M[A]
+
     @sum  // A -> R16
-    M=0   // Set R16 to 0
+    M=D   // Set R16 to value at D
+
 (LOOP)    // line 3 link for loop
     // Get first value
     @R0   // A -> R0
     D=M   // D->M[A]
 
-    // Add first value to second value
-    @R1   // A -> R1
+    // Add first value to sum (should be)
+    @sum   // A -> R16
     D=D+M // D -> D+A
 
     // Store combined value
