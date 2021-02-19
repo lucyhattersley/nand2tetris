98682a37ff75e1d96f1841f752b8962ad51c4b3a
lucyhattersley <lucy.a.hattersley@gmail.com>
2021-02-19 19:01:59 +0000

    Infinite loop. Count not increasing

--- test_code/Mult.asm
+++ test_code/Mult.asm
@@ -1,12 +1,25 @@
 // Multiplies R0 and R1
 // This code adds two numbers
+    @sum  // A -> R16
+    M=0   // Set R16 to 0
 (LOOP)    // line 3 link for loop
+    // Get first value
     @R0   // A -> R0
     D=M   // D->M[A]
 
+    // Add first value to second value
     @R1   // A -> R1
     D=D+M // D -> D+A
 
-    @R2  // A -> M[A]
-    M=D 
+    // Store combined value
+    @sum  // A -> R16
+    M=D   // M[A] -> R16
+
+    @LOOP // Points to loop
+    0;JMP // Jumps to loop
+
+(END)     // Infinite loop
+    @LOOP  // Points to END location
+    0;JMP // Jumps to END location
+
 
