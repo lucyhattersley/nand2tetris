588707ce1773180173fd7aea9cf2a99adab0f9fd
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-04-28 21:26:06 +0100

    Changed to regex approach@ ''

--- test_code/Parser/readfile.py
+++ test_code/Parser/readfile.py
@@ -1,5 +1,6 @@
-import sys # To get file name
+import sys # file support
 import os # for filename extension
+import re # regex support
 
 pre, ext = os.path.splitext(sys.argv[1])
 f = open(pre + ext, 'r')
@@ -7,8 +8,9 @@ o = open(pre + '.hack', 'w')
 input = f.readlines()
 
 for line in input:
-    if not line.strip().startswith(('//')): # gets rid of comments and blank lines
-        o.write("%s" % line)
+    # if not line.strip().startswith(('//')): # gets rid of comments and blank lines
+    if not re.match(r'^\s$', line): # (has only \t\b\r and whitespace)
+        o.write("%s" % line) 
 
 o.close()
 f.close()
