aa067c7bb9eaa3a60b2cdce8e8d724050dbc5b75
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-02 20:30:13 +0100

    Added advance function

--- test_code/Parser/parser.py
+++ test_code/Parser/parser.py
@@ -4,23 +4,29 @@ import re # regex support
 
 class Parser:
     def __init__(self, argv):
-        # open files
+        self.current_command = ''
+
+        # open file
         pre, ext = os.path.splitext(argv)
         f = open(pre + ext, 'r')
 
         # instantiate lists
         self.input = f.read()
 
-        # new clean code approach
+        # clean code 
         comment_free = re.sub('(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)|[^\S\r\n]', '', self.input) # removes comments
         lines = comment_free.splitlines() # splits into list
         self.input = [x for x in lines if x] # removes empty items / blank lines from list
 
-        # close files
+        # close file
         f.close()
     
     def hasMoreCommands(self):
         return(not not self.input)
+
+    def advance(self):
+        if self.hasMoreCommands():
+            self.current_command = self.input.pop(0)
 
 # init object 
 parser = Parser(sys.argv[1])
@@ -30,3 +36,7 @@ print(parser.input)
 
 # has more commands. should return True
 print(parser.hasMoreCommands())
+
+# get current command
+command = parser.advance()
+print('Current command is: ' + parser.current_command)
