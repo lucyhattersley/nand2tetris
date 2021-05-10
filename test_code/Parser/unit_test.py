976fb4b38bd8a2a31e8d2de65b22bbe0b1874c2a
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-10 12:07:53 +0100

    set up unit test

---
+++ test_code/Parser/unit_test.py
@@ -0,0 +1,10 @@
+import unittest
+import parser
+
+class TestSum(unittest.TestCase):
+    def has_more_commands_test(self):
+        self.assertEqual(test_parser.hasMoreCommands, True)
+
+if __name__ == "__main__":
+    test_parser = parser.Parser(argv="Max.asm")
+    unittest.main()
