768b3dd713df7f1e6b352460bfb3e850e7569a5e
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-12 09:42:58 +0100

    parser test setup

--- projects/06/parser_test.py
+++ projects/06/parser_test.py
@@ -1,10 +1,12 @@
 import unittest
 import parser
 
-class TestSum(unittest.TestCase):
-    def has_more_commands_test(self):
-        self.assertEqual(test_parser.hasMoreCommands, True)
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.test_parser = parser.Parser(argv="max/Max.asm")
+
+    def test_hasMoreCommands(self):
+        self.assertTrue(self.test_parser.hasMoreCommands())
 
 if __name__ == "__main__":
-    test_parser = parser.Parser(argv="max/Max.asm")
     unittest.main()
