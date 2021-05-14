5f0d6f644bd68073a1000198c9e97eb3ced8c75d
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-14 22:19:58 +0100

    added A tests; some c tests@

--- projects/06/test_parser.py
+++ projects/06/test_parser.py
@@ -8,9 +8,26 @@ class TestParser(unittest.TestCase):
     def test_hasMoreCommands(self):
         self.assertTrue(self.test_parser.hasMoreCommands())
 
-    def test_commandType(self):
-        self.test_parser.command = '@123'
-        self.assertEqual(self.test_parser.commandType(self), 'A_COMMAND')
+    
+    # A_COMMANDS
+    # @123 A_COMMAND
+    # @
+    # @TEST
 
+    # C_COMMANDS
+    # D=M
+    
+    # L_COMMANDS
+
+    def test_commandType(self):
+        tests = ['@', '@123', '@TEST']
+        for test in tests:
+            self.test_parser.command = test
+            self.assertEqual(self.test_parser.commandType(self), 'A_COMMAND')
+
+        tests = ['A=M', 'A=D']
+        for test in tests:
+            self.test_parser.command = test
+            self.assertEqual(self.test_parser.commandType(self), 'C_COMMAND')
 if __name__ == "__main__":
     unittest.main()
