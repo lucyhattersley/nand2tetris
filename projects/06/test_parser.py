bf4c67b2367ed8470b86fbdd5792b5aa85df8bb4
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-14 22:45:06 +0100

    C_COMMANDS passing

--- projects/06/test_parser.py
+++ projects/06/test_parser.py
@@ -15,7 +15,7 @@ class TestParser(unittest.TestCase):
             self.assertEqual(self.test_parser.commandType(self), 'A_COMMAND')
 
         tests = ['A=0' 'A=1', 'A=-1', 'A=D,' 'A=A', 'A=!D', 'A=!A', 'A=-D', 'A=-A', 'A=D+1', 'A=D-1', 'A=A-1', 'A=D+A', 'A=D-A', 
-                 'A=A-D', 'D&A', 'D|A', 'A=M', 'A=!M', 'A=-M', 'A=M+1', 'A=D+M', 'A=D-M', 'A=M-D', 'A=D&M', 'A=D|M']
+                 'A=A-D', 'A=D&A', 'A=D|A', 'A=M', 'A=!M', 'A=-M', 'A=M+1', 'A=D+M', 'A=D-M', 'A=M-D', 'A=D&M', 'A=D|M']
         for test in tests:
             self.test_parser.command = test
             self.assertEqual(self.test_parser.commandType(self), 'C_COMMAND', 'Failed: ' + test)
