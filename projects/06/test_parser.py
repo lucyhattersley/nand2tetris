2424bb5811f89a19d62eceb0ac527baea94aed37
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-14 22:41:31 +0100

    added A_Command tests

--- projects/06/test_parser.py
+++ projects/06/test_parser.py
@@ -25,7 +25,9 @@ class TestParser(unittest.TestCase):
             self.test_parser.command = test
             self.assertEqual(self.test_parser.commandType(self), 'A_COMMAND')
 
-        tests = ['A=M', 'A=D']
+        tests = ['A=0' 'A=1', 'A=-1', 'A=D,' 'A=A', 'A=!D', 'A=!A', 'A=-D', 'A=-A', 'A=D+1', 'A=D-1', 'A=A-1', 'A=D+A', 'A=D-A', 
+                 'A=A-D', 'D&A', 'D|A', 'A=M', 'A=!M', 'A=-M', 'A=M+1', 'A=D+M', 'A=D-M', 'A=M-D', 'A=D&M', 'A=D|M']
+
         for test in tests:
             self.test_parser.command = test
             self.assertEqual(self.test_parser.commandType(self), 'C_COMMAND')
