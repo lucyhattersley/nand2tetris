50ba06a5c5ca51cee0aa498dd8326cd4b3c3e595
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-05-28 16:06:40 +0100

    fleshing out assembler and test_assembler

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,3 +1,13 @@
 import parser
+import sys
 
-parser = Parser(sys.argv[1])
+class Assembler:
+    def __init__(self):
+        self.parser = Parser(sys.argv[1])
+        
+    def parse(self):
+        # input: expects ASM command
+        # returns a 16 digit Hack command
+        return 
+        
+assembler = Assembler()
--- projects/06/test_assembler.py
+++ projects/06/test_assembler.py
@@ -1,6 +1,26 @@
+import unittest
+import parser
+import assembler
+
+# @2
+# D=A
+# @3
+# D=D+A
+# @0
+# M=D
+
 # 0000000000000010
 # 1110110000010000
 # 0000000000000011
 # 1110000010010000
 # 0000000000000000
 # 1110001100001000
+
+class TestAssembler(unittest.TestCase):
+        def setUp(self):
+            self.test_code = assembler.Assembler()
+
+        def test_parse(self):
+            test = self.test_code.parse("@2")
+            self.assertEqual(test, '0000000000000010')
+
