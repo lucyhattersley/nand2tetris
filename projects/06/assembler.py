df65f384f6a4a2812f591ee39836f6c6c2764949
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-06-14 20:39:12 +0100

    moved symbol table to class

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -24,39 +24,16 @@ class Assembler:
         # instantiate objects
         self.parser = parser.Parser(self.input)
         self.code = parser.Code()
+        self.symbolTable = parser.SymbolTable()
 
-        # TODO move to symbolTable module
-        #Create symbol table with predefined symbols
-        self.symbol_table = {
-        "SP":"0000000000000000",
-        "LCL":"0000000000000001",
-        "ARG":"0000000000000002",
-        "THIS":"0000000000000003",
-        "THAT":"0000000000000004",
-        "R0":"0000000000000000",
-        "R1":"0000000000000001",
-        "R2":"0000000000000010",
-        "R3":"0000000000000011",
-        "R4":"0000000000000100",
-        "R5":"0000000000000101",
-        "R6":"0000000000000110",
-        "R7":"0000000000000111",
-        "R8":"0000000000001000",
-        "R9":"0000000000001001",
-        "R10":"0000000000001010",
-        "R11":"0000000000001011",
-        "R12":"0000000000001100",
-        "R13":"0000000000001101",
-        "R14":"0000000000001110",
-        "R15":"0000000000001111"
-        }
-         # Parse input stream
-         # We loop through input stream and add L_COMMANDS and binary index to Symbol Table
+        #  Parse input stream
+        # We loop through input stream and add L_COMMANDS and binary index to Symbol Table
         varAddr = 16
         for command in self.input:
             if self.parser.commandType(command) == 'L_COMMAND':
                 loc = "{0:016b}".format(self.input.index(command)) # convert index position to binary location
-                self.symbol_table[command] = loc
+                TODO: Use setters / getters here 
+                self.symboltable[command] = loc
                 self.input.remove(command) # Symbol Table location now points to next command in ASM (loop point)
             elif self.parser.commandType(command) == 'A_COMMMAND' and command[1].isalpha(): # Command is variable reference
                 loc = "{0:016b}".format(varAddr)
--- projects/06/parser.py
+++ projects/06/parser.py
@@ -127,3 +127,39 @@ class Code:
     
     def jump(self, mnemonic):
         return self.j_table[mnemonic]
+
+class SymbolTable:
+    def __init__(self):
+        #Create symbol table with predefined symbols
+        self.symbol_table = {
+        "SP":"0000000000000000",
+        "LCL":"0000000000000001",
+        "ARG":"0000000000000002",
+        "THIS":"0000000000000003",
+        "THAT":"0000000000000004",
+        "R0":"0000000000000000",
+        "R1":"0000000000000001",
+        "R2":"0000000000000010",
+        "R3":"0000000000000011",
+        "R4":"0000000000000100",
+        "R5":"0000000000000101",
+        "R6":"0000000000000110",
+        "R7":"0000000000000111",
+        "R8":"0000000000001000",
+        "R9":"0000000000001001",
+        "R10":"0000000000001010",
+        "R11":"0000000000001011",
+        "R12":"0000000000001100",
+        "R13":"0000000000001101",
+        "R14":"0000000000001110",
+        "R15":"0000000000001111"
+        }
+        
+    def addEntry(symbol, address):
+        self.symbol_table[symbol] = address
+
+    def contains(symbol):
+        return symbol in self.symbol_table
+
+    def getAddress(symbol):
+        return self.symbol_table[symbol]
