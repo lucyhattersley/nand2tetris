b33f9bc6bcb3da30daa54778acd4670d24c5dcf1
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-07-02 22:14:51 +0100

    moved return True

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -40,7 +40,9 @@ class Assembler:
                 loc = "{0:016b}".format(varAddr)
                 self.symbolTable.addEntry(command, address)
                 varAddr += 1
-                return True
+            
+            return True
+        
         self.input[:] = [command for command in self.input if not determine(command)]
 
     def parse(self, line):
