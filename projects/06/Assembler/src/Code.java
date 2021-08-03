d0ff56a4470f0ae9cc906a3450ac685173498bfd
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-03 18:22:34 +0100

    Added j_table

--- projects/06/Assembler/src/Code.java
+++ projects/06/Assembler/src/Code.java
@@ -6,6 +6,9 @@ public class Code {
 
     private  static Hashtable<String, String> d_table = 
             new Hashtable<String, String>();
+    
+    private  static Hashtable<String, String> j_table = 
+            new Hashtable<String, String>();
 
     static {
 	    c_table.put("0","0101010");
@@ -49,6 +52,17 @@ public class Code {
     	d_table.put("AD","110");
     	d_table.put("AMD","111");
     }
+    
+    static {
+    	j_table.put("null","000");
+    	j_table.put("JGT","001");
+    	j_table.put("JEQ","010");
+    	j_table.put("JGE","011");
+    	j_table.put("JLT","100");
+    	j_table.put("JNE","101");
+    	j_table.put("JLE","110");
+    	j_table.put("JMP","111");
+    }
 
     public String comp(String mnem) {
     	return c_table.get(mnem);
@@ -56,6 +70,10 @@ public class Code {
 
     public String dest(String mnem) {
     	return d_table.get(mnem);
+    }
+
+    public String jump(String mnem) {
+    	return j_table.get(mnem);
     }
  
 }
