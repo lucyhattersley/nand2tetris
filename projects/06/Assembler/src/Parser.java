dcab0b314ef89e8fc7386efe315bc4beae49b638
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-04 15:13:50 +0100

    added BufferedReader to parser. Create TestParser

--- projects/06/Assembler/src/Parser.java
+++ projects/06/Assembler/src/Parser.java
@@ -1,3 +1,16 @@
-public class Parser {
+import java.io.*;
 
+public class Parser {
+	
+	private static void initialize(String filename) {
+		try{
+			File myFile = new File(filename);
+			FileReader fr = new FileReader(myFile);
+			BufferedReader br = new BufferedReader(fr);
+			
+			br.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
---
+++ projects/06/Assembler/src/TestParser.java
@@ -0,0 +1,10 @@
+public class TestParser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String testFile = "add/Add.asm";
+		Parser myParser = new Parser(testFile);
+
+	}
+
+}
