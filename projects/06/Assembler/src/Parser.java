d94d5a4c57ebdbda0243606d246ebc73e07c5818
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-04 18:12:47 +0100

    initializer not stripping comments correctly

--- projects/06/Assembler/src/Parser.java
+++ projects/06/Assembler/src/Parser.java
@@ -10,13 +10,28 @@ public class Parser {
 			
 			// Test code to print contents of file
 			String line = null;
+			String newLine = null;
+			char comment = '/';
+
 			while ((line = br.readLine()) != null) {
-				// System.out.println(line);
+				boolean finished = false;
 
 				// now let's remove comments
-				for (char c : line.toCharArray()) {
-					System.out.print(Character.toString(c));
+				for (int i=0; i < line.length()-1;i++) {
+					char c = line.charAt(i);
+					char d = line.charAt(i+1); 
+					if (!finished) {
+						if (c == comment && d == comment) {
+							finished = true; // we found a comment so stop writing to the newLine
+							break;
+						} else {
+							newLine += Character.toString(c); // write the char to the newLine
+						}
+					}
+
 				}
+				
+				System.out.println(newLine); // let's see if it worked
 			}
 
 
