51d12a1a8b95f22d3d567b666ba1140089750176
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-04 17:21:14 +0100

    iterate over chars in string

--- projects/06/Assembler/src/Parser.java
+++ projects/06/Assembler/src/Parser.java
@@ -8,10 +8,18 @@ public class Parser {
 			FileReader fr = new FileReader(myFile);
 			BufferedReader br = new BufferedReader(fr);
 			
+			// Test code to print contents of file
 			String line = null;
 			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+				// System.out.println(line);
+
+				// now let's remove comments
+				for (char c : line.toCharArray()) {
+					System.out.print(Character.toString(c));
+				}
 			}
+
+
 			
 			br.close();
 		} catch (Exception ex) {
