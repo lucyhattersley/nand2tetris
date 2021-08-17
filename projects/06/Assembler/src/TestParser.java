be4c0d3bce8ba242a751e2f9006a3c6de32fb260
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-17 12:38:30 +0100

    Setting up tests in Eclipse

---
+++ projects/06/Assembler/src/TestParser.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import junit.framework.*;
+import org.junit.Test;
+
+public class TestParser extends TestCase {
+	
+	public TestParser(String name) {
+		super(name);
+	}
+
+	@Test
+	public void testParser() {
+		Parser parser = new Parser();
+		
+	}
+
+}
