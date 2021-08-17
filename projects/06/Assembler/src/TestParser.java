723d3d9bdf9d8a1da8d985c68ea14faf5d319d1e
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-08-17 12:59:25 +0100

    TestParser up and running

--- projects/06/Assembler/src/TestParser.java
+++ projects/06/Assembler/src/TestParser.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 import junit.framework.*;
 import org.junit.Test;
+import java.util.*;
+
 
 public class TestParser extends TestCase {
 	
@@ -9,9 +11,16 @@ public class TestParser extends TestCase {
 	}
 
 	@Test
-	public void testParser() {
-		Parser parser = new Parser();
+	public void testInput() {
+		Parser testParser = new Parser();
 		
+		String testFile = "/Users/lucy/nand2tetris/projects/06/add/Add.asm"; 
+		testParser.initialize(testFile);
+		
+		ArrayList<String> testInput = new ArrayList<String>(Arrays.asList("@2", "D=A", "@3", "D=D+A", "@0", "M=D"));
+		
+		assertEquals(testInput, testParser.input);
+
 	}
 
 }
