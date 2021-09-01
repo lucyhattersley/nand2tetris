608ead36bcaf14131e56ce034ef51e5b91374728
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-09-01 10:42:23 +0100

    Added notEquals test

--- projects/06/Assembler/src/TestAssembler.java
+++ projects/06/Assembler/src/TestAssembler.java
@@ -23,14 +23,25 @@ public class TestAssembler {
 
 		// Load Add1.hack
 		//		String add1File = "/Users/lucy/nand2tetris/projects/06/add/Add.hack";
-		FileReader fr2 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add.hack");
+		FileReader fr2 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add1.hack");
 		BufferedReader br2 = new BufferedReader(fr2);
 		String response2 = new String();
 		for(String line; (line=br2.readLine()) != null; response2 += line); 
 		br2.close();
+		
+		// Addbad.hack (different file, should fail)
+		FileReader fr3 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Addbad.hack");
+		BufferedReader br3 = new BufferedReader(fr3);
+		String response3 = new String();
+		for(String line; (line=br3.readLine()) != null; response3 += line); 
+		br3.close();
+		
 
 		// Diff files
 		assertEquals(response, response2);
+		
+		// Diff files
+		assertNotEquals(response, response3);
 	}
 	
 	/*
