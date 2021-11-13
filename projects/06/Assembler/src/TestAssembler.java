8442dcf049bd81bd50acd05314d761e2a815528d
Lucy Hattersley <lucyahattersley@gmail.com>
2021-11-13 22:43:49 +0000

    Updated filename. Needs at add output

--- projects/06/Assembler/src/TestAssembler.java
+++ projects/06/Assembler/src/TestAssembler.java
@@ -207,15 +207,14 @@ public class TestAssembler {
 		assembler.output(addAsm);
 		
 		// Load Add.hack
-		FileReader fr = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add.hack");
+		FileReader fr = new FileReader("../add/Add.hack");
 		BufferedReader br = new BufferedReader(fr);
 		String response = new String();
 		for(String line; (line=br.readLine()) != null; response += line); 
 		br.close();
 				
 		// Load Add1.hack
-		String add1File = "/Users/lucy/nand2tetris/projects/06/add/Add.hack";
-		FileReader fr2 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add1.hack");
+		FileReader fr2 = new FileReader("../add/Add1.hack");
 		BufferedReader br2 = new BufferedReader(fr2);
 		String response2 = new String();
 		for(String line; (line=br2.readLine()) != null; response2 += line); 
