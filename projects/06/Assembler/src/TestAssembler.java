1a25042e2cec31d75154b6eaf8f8db6cc35f341f
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2021-11-13 12:40:05 +0000

    testAssembler back. Fails: Assembler not creating output file

--- projects/06/Assembler/src/TestAssembler.java
+++ projects/06/Assembler/src/TestAssembler.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.ArrayList;
+import java.io.*;
 
 public class TestAssembler {
 
@@ -194,41 +195,33 @@ public class TestAssembler {
 
 	}
 	
+	@Test
+	public void testAssembler() throws Exception {
+		Assembler assembler = new Assembler();
 
-//	@Test
-//	public void testAssembler() throws Exception {
-//		assertNotNull(myAssembler);
-//		
-//		// Load Add.hack
-//		FileReader fr = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add.hack");
-//		BufferedReader br = new BufferedReader(fr);
-//		String response = new String();
-//		for(String line; (line=br.readLine()) != null; response += line); 
-//		br.close();
+		assembler.firstPass(maxAsm);
+		assembler.secondPass();
+		
+		// Load Add.hack
+		FileReader fr = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add.hack");
+		BufferedReader br = new BufferedReader(fr);
+		String response = new String();
+		for(String line; (line=br.readLine()) != null; response += line); 
+		br.close();
 				
 		// Load Add1.hack
-		//		String add1File = "/Users/lucy/nand2tetris/projects/06/add/Add.hack";
-		// FileReader fr2 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add1.hack");
-		// BufferedReader br2 = new BufferedReader(fr2);
-		// String response2 = new String();
-		// for(String line; (line=br2.readLine()) != null; response2 += line); 
-		// br2.close();
-		
-		// Addbad.hack (different file, should fail)
-		// FileReader fr3 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Addbad.hack");
-		// BufferedReader br3 = new BufferedReader(fr3);
-		// String response3 = new String();
-		// for(String line; (line=br3.readLine()) != null; response3 += line); 
-		// br3.close();
-		
+		String add1File = "/Users/lucy/nand2tetris/projects/06/add/Add.hack";
+		FileReader fr2 = new FileReader("/Users/lucy/nand2tetris/projects/06/add/Add1.hack");
+		BufferedReader br2 = new BufferedReader(fr2);
+		String response2 = new String();
+		for(String line; (line=br2.readLine()) != null; response2 += line); 
+		br2.close();		
 
 		// Diff files
-//		assertEquals(response, response2);
-//		
-//		// Diff files
-//		assertNotEquals(response, response3);
-//	}
-//	
+		assertEquals(response, response2);
+		
+	}
+	
 	/*
 	 * Assembler should initalize with the following
 	 * SP 0
