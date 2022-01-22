d6655a8f674f5d70d76483b1907a62df28051151
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2022-01-22 22:30:25 +0000

    Got junit working. testInput failing

--- projects/07/VMTranslator/src/TestParser.java
+++ projects/07/VMTranslator/src/TestParser.java
@@ -2,20 +2,17 @@ import junit.framework.*;
 import org.junit.Test;
 import java.util.*;
 
-public class TestParser extends TestCase {
-   @Test
-    public void TestParser(String name) {
-        super(name); // Test creation
-    }
+public class TestParser extends TestCase  {
 
     //Setup
-	String testFile = "../../StackArithmetic/SimpleAdd/SimpleAdd.vm";
+	String testFile = "/Users/lucy/nand2tetris/projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm";
 	Parser myParser = new Parser();	
 
     @Test
 	public void testInput() {
 		myParser.initialize(testFile);
         ArrayList<String> testInput = new ArrayList<String>(Arrays.asList("push constant 7","push constant 8", "add"));
+        assertEquals(testInput, myParser.input);
 
     }
 
