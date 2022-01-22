d3a13ac68a8055e0062c2ba5149bc94486231feb
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2022-01-22 21:41:23 +0000

    Add testInput code

--- projects/07/VMTranslator/src/TestParser.java
+++ projects/07/VMTranslator/src/TestParser.java
@@ -3,8 +3,20 @@ import org.junit.Test;
 import java.util.*;
 
 public class TestParser extends TestCase {
-    public TestParser(String name) {
+   @Test
+    public void TestParser(String name) {
         super(name); // Test creation
+    }
+
+    //Setup
+	String testFile = "../../StackArithmetic/SimpleAdd/SimpleAdd.vm";
+	Parser myParser = new Parser();	
+
+    @Test
+	public void testInput() {
+		myParser.initialize(testFile);
+        ArrayList<String> testInput = new ArrayList<String>(Arrays.asList("push constant 7","push constant 8", "add"));
+
     }
 
 }
