c14f3e4d6f0cfe7c25d9cbdf8688642fc028954d
lucy <lucyahattersley@gmail.com>
2022-02-16 21:45:31 +0000

    reverting to 84492e2c40e7d9c11cab9504a5a71e4f65eb1baf

---
+++ projects/07/VMTranslator/src/CodeWriter.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeWriter {
+    String fileName;
+
+    /**
+     * Opens the output filestream and gets ready to write into it
+     * @param output filestream. The file to save the filestream to
+     * @return none
+     */
+    public void Constructor(String output) {
+        try {
+            File myFile = new File(output);
+            FileWriter fw = new FileWriter(myFile);    
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            }
+    }
+
+    /**
+     * Informs the code writer that the translation of a new VM file is started
+     * @param fileName
+     */
+    public void setFileName(String fileName) {
+        fileName = fileName;
+    }
+
+    /**
+     * Writes the assembly code that is the translation of the given arithmetic command
+     * @param command
+     * @return
+     */
+    public void writeArithmetic(String command) {
+        //TODO write assembly of command to String
+        System.out.println("Running writeArithmetic");
+    }
+
+    /**
+     * Closes the output file
+     */
+    public void close() {
+        // fw.close(); Todo. Close FW
+    }
+}
---
+++ projects/07/VMTranslator/src/TestParser.java
@@ -0,0 +1,96 @@
+import junit.framework.*;
+import org.junit.Test;
+import java.util.*;
+
+public class TestParser extends TestCase  {
+
+    // WSL Linux
+    String testFile = "/home/lucy/nand2tetris/projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm";
+    Parser myParser = new Parser();
+
+    @Test
+	public void testInput() {
+        myParser.initialize(testFile);
+        ArrayList<String> testInput = new ArrayList<String>(Arrays.asList("push constant 7","push constant 8", "add"));
+        assertEquals(testInput, myParser.input);
+
+    }
+
+    @Test
+    public void testHasMoreCommands() {
+        myParser.initialize(testFile);
+        assertTrue(myParser.hasMoreCommands());
+    }
+
+    @Test
+    public void testCurrentCommand() {
+        myParser.initialize(testFile);
+        myParser.advance();
+        String testInput = "push constant 7";
+        assertEquals(testInput, myParser.getCurrentCommand());
+    }
+
+    @Test
+    public void testCommandPush() {
+        myParser.initialize(testFile);
+        myParser.advance();
+        String testCommandType = "C_PUSH";
+        assertEquals(testCommandType, myParser.commandType());
+    }
+
+    @Test
+    public void testCommandPull() {
+        myParser.initialize(testFile);
+        myParser.currentCommand = "pull arg1 arg2";
+        String testCommandType = "C_PULL";
+        assertEquals(testCommandType, myParser.commandType());
+
+    }
+
+    @Test
+    public void testCommandArithmetic() {
+        myParser.currentCommand = "add arg1 arg2";
+        String testCommandAdd = "C_ARITHMETIC";
+        assertEquals(testCommandAdd, myParser.commandType());
+
+        myParser.currentCommand = "sub arg1 arg2";
+        String testCommandSub = "C_ARITHMETIC";
+        assertEquals(testCommandSub, myParser.commandType());
+
+        myParser.currentCommand = "neg arg1 arg2";
+        String testCommandNeg = "C_ARITHMETIC";
+        assertEquals(testCommandNeg, myParser.commandType());
+
+    }
+
+    @Test
+    public void testArg1() {
+        myParser.currentCommand = "push constant 7";
+        String testArg1Constant = "constant";
+        assertEquals(testArg1Constant, myParser.arg1());
+
+        myParser.currentCommand = "add";
+        String testArg1Add = "add";
+        assertEquals(testArg1Add, myParser.arg1());
+
+
+    }
+
+    @Test
+    public void testArg2() {
+        myParser.currentCommand = "push constant 7";
+        int testArg2Constant = 7;
+        assertEquals(testArg2Constant, myParser.arg2());
+    }
+
+    // TODO Chapter 8
+    // @Test 
+    // public void testCommandLabel() {
+    //     myParser.currentCommand = "add arg1 arg2";
+    //     String testCommandAdd = "C_ARITHMETIC";
+    //     assertEquals(testCommandAdd, myParser.commandType());
+
+    // }
+
+
+}
