2d9fe634823c38b5cb2445235bea188fc9447e63
lucy <lucyahattersley@gmail.com>
2022-02-19 19:45:05 +0000

    Created filediff test

--- projects/07/VMTranslator/src/test/java/TestCodeWriter.java
+++ projects/07/VMTranslator/src/test/java/TestCodeWriter.java
@@ -1,13 +1,15 @@
 import junit.framework.*;
 import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 import java.io.*;
 import java.nio.file.*; 
 
 public class TestCodeWriter extends TestCase {
     
-    // String testFile = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestSimpleAdd.asm"; // WSL
-    String testFile = "/Users/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestSimpleAdd.asm"; // Mac
+    String testFile = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestSimpleAdd.asm"; // WSL
+    //String testFile = "/Users/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestSimpleAdd.asm"; // Mac
     CodeWriter myCodeWriter = new CodeWriter();
 
     @Test
@@ -38,7 +40,7 @@ public class TestCodeWriter extends TestCase {
      */
     @Test
     public void testWriteArithmeticPushConstant7() {
-        String outputFile = "/Users/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestPushConstant7.asm"; // Mac
+        String outputFile = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestPushConstant7.asm"; // WSL
         CodeWriter myCodeWriter = new CodeWriter();
         myCodeWriter.Constructor(outputFile); // creates output file
    
@@ -46,15 +48,51 @@ public class TestCodeWriter extends TestCase {
         String command = "push constant 7";
         myCodeWriter.writeArithmetic(command);
                 
-        try {
-            FileReader fr = new FileReader(outputFile);
-            BufferedReader br = new BufferedReader(fr);
-            // TODO: import TestPushConstant7Expected.asm
-            // TODO diff outputFile and TestPushConstant7Expected.asm
+        try {            
+            String input = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestPushConstant7.vm";
+            String output = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestPushConstant7.asm";
+            String expectedOutput = "/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/TestPushConstant7Expected.asm";
+
+            BufferedReader rOutput = new BufferedReader(new FileReader(output));
+            BufferedReader rExpectedOutput = new BufferedReader(new FileReader(expectedOutput));
+
+            List<String> lOutput = new ArrayList<String>();
+            List<String> lExpectedOutput = new ArrayList<String>();
+            
+            // Parse output to list
+            String sCurrentLine;
+
+            while ((sCurrentLine = rOutput.readLine()) != null) {
+                lOutput.add(sCurrentLine);
+            }
+
+            // Parse ExcepctedOutout to list
+            while ((sCurrentLine = rExpectedOutput.readLine()) != null) {
+                lExpectedOutput.add(sCurrentLine);
+            }
+
+            assertEquals(lExpectedOutput, lOutput);
+
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
+
+    // Diff files code from https://www.baeldung.com/java-compare-files
+    // @Test
+    // public void whenFilesIdenticalIgnoreEOF_thenReturnTrue() throws IOException {
+    //     Path path1 = Files.createTempFile("file1Test", ".txt");
+    //     Path path2 = Files.createTempFile("file2Test", ".txt");
+    
+    //     Files.writeString(path1, "testing line 1 \n line 2");
+    //     Files.writeString(path2, "testing line 1 \r\n line 2");
+    
+    //     Reader reader1 = new BufferedReader(new FileReader(path1.toFile()));
+    //     Reader reader2 = new BufferedReader(new FileReader(path2.toFile()));
+    
+    //     assertTrue(IOUtils.contentEqualsIgnoreEOL(reader1, reader2));
+    // }
+
 
     //Teardown
     // @Test
