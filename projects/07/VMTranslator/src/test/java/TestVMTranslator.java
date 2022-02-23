627d89104de21712934aac1b98254ee98460c21d
Lucy Hattersley <lucyahattersley@gmaill.com>
2022-02-23 09:19:51 +0000

    main test setup

--- projects/07/VMTranslator/src/test/java/TestVMTranslator.java
+++ projects/07/VMTranslator/src/test/java/TestVMTranslator.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-
+import java.io.*;
+import java.util.*;
 import junit.framework.TestCase;
 
 public class TestVMTranslator extends TestCase {
@@ -12,9 +13,36 @@ public class TestVMTranslator extends TestCase {
      */
     @Test
     public void testMain() {
-        String simpleAddExpected = new String("")
-        //TODO String SimpleAddExpected.asm
-        //TODO String SimpleAdd.asm
+        String simpleAddExpected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/SimpleAddExpected.asm");
+        String simpleAdd = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/SimpleAdd.asm");
+
+        //Todo initialise Parser, CodeWriter and parse SimpleAdd 
+
+        try {
+            BufferedReader brSimpleAddExpected = new BufferedReader(new FileReader(simpleAddExpected));
+            BufferedReader brSimpleAdd = new BufferedReader(new FileReader(simpleAdd));
+    
+            // Check contents of both files
+            List<String> lExpectedOutput = new ArrayList<String>();
+            List<String> lOutput = new ArrayList<String>();
+            
+            String sCurrentLine;
+    
+            // Parse SimpleAddExpected to list
+            while ((sCurrentLine = brSimpleAddExpected.readLine()) != null) {
+                lExpectedOutput.add(sCurrentLine);
+            }
+    
+            // Parse SimpleAdd to list
+            while ((sCurrentLine = brSimpleAdd.readLine()) != null) {
+                lOutput.add(sCurrentLine);
+            }
+    
+            assertEquals(lExpectedOutput, lOutput);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
