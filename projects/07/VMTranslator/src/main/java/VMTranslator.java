9e0a13bae8b49f0796d61023316893c06ed12143
Lucy Hattersley <lucyahattersley@gmaill.com>
2022-02-25 08:49:06 +0000

    Passed arg into parser init

--- projects/07/VMTranslator/src/main/java/VMTranslator.java
+++ projects/07/VMTranslator/src/main/java/VMTranslator.java
@@ -3,7 +3,11 @@ public class VMTranslator {
     public static void main(String[] args) {
         System.out.println("Running VMTranslator.main");
         
+        // Create objects
         Parser myParser = new Parser();
         CodeWriter myCodeWriter = new CodeWriter();
+
+        // Init parser with arg
+        myParser.initialize(args[0]);
     }
 }
