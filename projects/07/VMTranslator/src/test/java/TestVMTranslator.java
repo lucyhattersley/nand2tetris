aed83178a8331a25523e9f2775a2fb57e140d241
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2022-03-31 09:10:46 +0100

    Implemented testRunner (DRY)

--- projects/07/VMTranslator/src/test/java/TestVMTranslator.java
+++ projects/07/VMTranslator/src/test/java/TestVMTranslator.java
@@ -14,69 +14,49 @@ public class TestVMTranslator extends TestCase {
     @Test
     public void testMainPushConstant7() {
         // File locations
-        String pushConstant7Expected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7Expected.asm");
-        String pushConstant7vm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7.vm");
-        String pushConstant7asm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7.asm");
+        String expected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7Expected.asm");
+        String vm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7.vm");
+        String asm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant7.asm");
 
-        // Set up VMTranslator
-        VMTranslator myVMTranslator = new VMTranslator();
-        
-        // Pass file location to main
-        String[] args = new String[1];
-        args[0] = pushConstant7vm;
-        myVMTranslator.main(args);
-
-        // Check file output
-        try {
-            BufferedReader brExpected = new BufferedReader(new FileReader(pushConstant7Expected));
-            BufferedReader brASM = new BufferedReader(new FileReader(pushConstant7asm));
-    
-            // Check contents of both files
-            List<String> lExpectedOutput = new ArrayList<String>();
-            List<String> lASM = new ArrayList<String>();
-            
-            String sCurrentLine;
-    
-            // Parse Expected to list
-            while ((sCurrentLine = brExpected.readLine()) != null) {
-                lExpectedOutput.add(sCurrentLine);
-            }
-    
-            // Parse ASM to list
-            while ((sCurrentLine = brASM.readLine()) != null) {
-                lASM.add(sCurrentLine);
-            }
-
-            // Check equality
-            assertEquals(lExpectedOutput, lASM);
-    
-            brASM.close();
-            brExpected.close();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Call Runner
+        testRunner(expected, vm, asm);
     }
 
     @Test
     public void testMainPushConstant78() {
         // File locations
-        String pushConstant78Expected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78Expected.asm");
-        String pushConstant78vm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78.vm");
-        String pushConstant78asm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78.asm");
+        String expected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78Expected.asm");
+        String vm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78.vm");
+        String asm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78.asm");
 
+         // Call Runner
+         testRunner(expected, vm, asm);
+    }
+
+    @Test
+    public void test78Add() {
+        // File locations
+        String expected = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78AddExpected.asm");
+        String vm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78Add.vm");
+        String asm = new String("/home/lucy/nand2tetris/projects/07/VMTranslator/src/test/resources/PushConstant78Add.asm");
+
+        // Call Runner
+        testRunner(expected, vm, asm);
+    }
+
+    public void testRunner(String expected, String vm, String asm) {
         // Set up VMTranslator
         VMTranslator myVMTranslator = new VMTranslator();
         
         // Pass file location to main
         String[] args = new String[1];
-        args[0] = pushConstant78vm;
+        args[0] = vm;
         myVMTranslator.main(args);
 
         // Check file output
         try {
-            BufferedReader brExpected = new BufferedReader(new FileReader(pushConstant78Expected));
-            BufferedReader brASM = new BufferedReader(new FileReader(pushConstant78asm));
+            BufferedReader brExpected = new BufferedReader(new FileReader(expected));
+            BufferedReader brASM = new BufferedReader(new FileReader(asm));
     
             // Check contents of both files
             List<String> lExpectedOutput = new ArrayList<String>();
