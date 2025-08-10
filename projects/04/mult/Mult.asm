24c366eb07a0bfd69147097a21cc4fc375f965bb
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2025-08-10 10:53:18 +0100

    move projects to projects v1

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,37 +1,9 @@
-// Multiplies R0 and R1
-    @R2   // Initialize R2 to 0
-    M=0
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
 
-(LOOP)    // Link for loop
-    // Check R1 > 0. If R1 <=0 GOTO END  
-    @R1   // A-R1
-    D=M   // D -> M[A]
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-    @END  // Point to END
-    D;JLE // Jump to end if D<=0
-
-    // Get first value
-    @R0   // A -> R0
-    D=M   // D->M[A]
-
-    // Add first value to sum (should be)
-    @R2  // A -> R16
-    D=D+M // D -> D+A
-
-    // Store value
-    @R2  // A -> R16
-    M=D   // M[A] -> R16
-
-    // Reduce R1 by 1
-    @R1   // A -> R1
-    D=M-1 // Stores val in D
-
-    @R1   // A -> R1
-    M=D   // Stores val in R1
-
-    @LOOP // Points to loop
-    0;JMP // Jumps to loop
-
-(END)     // Infinite loop
-    @END  // Points to END location
-    0;JMP // Jumps to END location
+// Put your code here.
